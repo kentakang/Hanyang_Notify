eca80cf406ae7fbe499c7a9d230a1fe45cbb8012
kentakang <kentakang@icloud.com>
2019-06-22 14:56:21 +0900

    Add react-native-vector-icons

--- react_native_app/android/app/src/main/java/com/react_native_app/MainActivity.java
+++ react_native_app/android/app/src/main/java/com/react_native_app/MainActivity.java
@@ -23,6 +23,6 @@ public class MainActivity extends ReactActivity {
             protected ReactRootView createRootView() {
                 return new RNGestureHandlerEnabledRootView(MainActivity.this);
             }
-        }
+        };
     }
 }
