166c9cb896b2f51fb0a965f35d11cc91bd292cdf
kentakang <kentakang@icloud.com>
2019-06-28 00:55:32 +0900

    Add icon on Document page.

--- react_native_app/pages/Document/index.js
+++ react_native_app/pages/Document/index.js
@@ -23,14 +23,21 @@ const TitleBar = styled.View`
   width: 100%;
   height: 9%;
   background: #007ac1;
-  justify-content: center;
+  align-items: center;
+  flex-direction: row;
 `;
 
 const Title = styled.Text`
   color: #ffffff;
-  margin-left: 5%;
   font-weight: bold;
   font-size: 18;
+  margin-left: 3%;
+`;
+
+const TitleIcon = styled.Image`
+  width: 18;
+  height: 18;
+  margin-left: 5%;
 `;
 
 const StyledCard = styled(Card)`
@@ -57,6 +64,7 @@ const Document = ({ navigation }) => {
     <Container>
       <StatusBar backgroundColor="#007ac1" barStyle="light-content" />
       <TitleBar>
+        <TitleIcon source={require('../../resources/images/Document.png')} resizeMode="contain" />
         <Title>가정통신문</Title>
       </TitleBar>
       {isLoading ? (
